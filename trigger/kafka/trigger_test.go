f756e6a5204a78d2b2259c52df8ec55d466a618c
Xiangru Liang <xliang@wayfair.com>
2019-10-17 16:16:36 -0400

    update structure

--- trigger/kafka/trigger_test.go
+++ trigger/kafka/trigger_test.go
@@ -1,52 +1,136 @@
 package kafka
 
 import (
+	"context"
 	"encoding/json"
+	"io/ioutil"
 	"testing"
+	"time"
 
-	"github.com/project-flogo/core/action"
-	"github.com/project-flogo/core/support/test"
-	"github.com/project-flogo/core/trigger"
-	"github.com/stretchr/testify/assert"
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"github.com/optiopay/kafka"
+	"github.com/optiopay/kafka/proto"
 )
 
+var jsonMetadata = getJsonMetadata()
+var ranAction = make(chan bool, 1)
+
+func getJsonMetadata() string {
+	jsonMetadataBytes, err := ioutil.ReadFile("trigger.json")
+	if err != nil {
+		panic("No Json Metadata found for trigger.json path")
+	}
+	return string(jsonMetadataBytes)
+}
+
 const testConfig string = `{
-	"id": "flogo-timer",
-	"ref": "github.com/project-flogo/contrib/trigger/kafka",
-	"settings": {
-	  "brokerUrls": "localhost:9092"
-	},
-	"handlers": [
-	  {
-			"action":{
-				"id":"dummy"
-			},
-			"settings": {
-		  	"topic": "syslog"
-			}
-	  }
-	]
+  "name": "kafka",
+  "settings": {
+    "server": "127.0.0.1:9092",
+    "configid": "test-flogo-trigger"
+  },
+  "handlers": [
+    {
+      "actionId": "local://testFlow",
+      "settings": {
+        "topic": "test",
+				"partition": "0"
+      }
+    },
+		{
+      "actionId": "local://testFlow2",
+      "settings": {
+        "topic": "flogo",
+				"partition": "0"
+      }
+    }
+  ]
+}`
 
-  }`
+var kafkaAddrs = []string{"127.0.0.1:9092"}
 
-func TestKafkaTrigger_Initialize(t *testing.T) {
-	f := &Factory{}
+type TestRunner struct {
+}
 
-	config := &trigger.Config{}
-	err := json.Unmarshal([]byte(testConfig), config)
-	assert.Nil(t, err)
+// Run implements action.Runner.Run
+func (tr *TestRunner) Run(context context.Context, action action.Action, uri string, options interface{}) (code int, data interface{}, err error) {
+	log.Debugf("Ran Action: %v", uri)
+	return 0, nil, nil
+}
 
-	actions := map[string]action.Action{"dummy": test.NewDummyAction(func() {
-		//do nothing
-	})}
+func (tr *TestRunner) RunAction(ctx context.Context, act action.Action, options map[string]interface{}) (results map[string]*data.Attribute, err error) {
+	log.Debugf("Ran Action: %v", act.Config().Id)
+	return nil, nil
+}
 
-	trg, err := test.InitTrigger(f, config, actions)
-	assert.Nil(t, err)
-	assert.NotNil(t, trg)
+/*func TestInit(t *testing.T) {
+	log.Info("Testing Init")
+	config := trigger.Config{}
+	json.Unmarshal([]byte(testConfig), &config)
+	f := &kafkaFactory{}
+	tgr := f.New(&config)
+	runner := &TestRunner{}
+	tgr.Init(runner)
+} */
 
-	err = trg.Start()
-	assert.Nil(t, err)
-	err = trg.Stop()
-	assert.Nil(t, err)
+func TestEndpoint(t *testing.T) {
+	log.Info("Testing Endpoint")
+	config := trigger.Config{}
+	json.Unmarshal([]byte(testConfig), &config)
+	// New  factory
+	f := &kafkaFactory{}
+	f.metadata = trigger.NewMetadata(jsonMetadata)
+	tgr := f.New(&config)
+	runner := &TestRunner{}
+	tgr.Init(runner)
+	tgr.Start()
+	defer tgr.Stop()
+
+	conf := kafka.NewBrokerConf("test-client")
+	conf.AllowTopicCreation = true
+
+	// connect to kafka cluster
+	broker, err := kafka.Dial(kafkaAddrs, conf)
+	if err != nil {
+		t.Error("Error connecting to kafka cluster")
+		t.Fail()
+	}
+	defer broker.Close()
+
+	producer := broker.Producer(kafka.NewProducerConf())
+	message := "Test message from Flogo"
+	msg := &proto.Message{Value: []byte(message)}
+	log.Info("Publishing test message to topic 'test' on Kafka")
+	if _, err := producer.Produce("test", 0, msg); err != nil {
+		t.Error("Error sending message to topic test")
+		t.Fail()
+	}
+	//broker.Close()
+	log.Info("Waiting 5 seconds for the message to be handled by the trigger...")
+	select {
+	case <-ranAction:
+		log.Debug("Message was handled OK by the trigger")
+	case <-time.After(5 * time.Second):
+		t.Error("No action called by trigger based on message")
+		t.Fail()
+		return
+	}
+	log.Info("Publishing test message to topic 'flogo' on Kafka")
+	if _, err := producer.Produce("flogo", 0, msg); err != nil {
+		t.Error("Error sending message to topic flogo")
+		t.Fail()
+	}
+	//broker.Close()
+	log.Info("Waiting 5 seconds for the message to be handled by the trigger...")
+	select {
+	case <-ranAction:
+		log.Debug("Message was handled OK by the trigger")
+	case <-time.After(5 * time.Second):
+		t.Error("No action called by trigger based on message")
+		t.Fail()
+		return
+	}
 
 }
